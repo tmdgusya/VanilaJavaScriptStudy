e4b22774af120055226b68e5aedac5a75f9a278e
roach <roach@cleaninglab.co.kr>
2021-07-18 10:24:27 +0900

    색깔바꾸기 기능 추가

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+const context = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColors");
+
+canvas.width = 700;
+canvas.height = 700;
+
+context.strokeStyle = "#2c2c2c";
+context.lineWidth = 2.5;
+
 
 let painting = false;
 
@@ -6,22 +15,32 @@ function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event) {
-    const screenX = event.offsetX;
-    const screenY = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function handleColorClick(event) {
+    context.strokeStyle = event.target.style.backgroundColor;
+}
+
+function onMouseMove(event) {
+    const screenX = event.offsetX;
+    const screenY = event.offsetY;
+    if (!painting) {
+        context.beginPath();
+        context.moveTo(screenX, screenY);
+    }
+
+    if(painting) {
+        context.lineTo(screenX, screenY);
+        context.stroke();    }
 }
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove)
-    canvas.addEventListener("mousedown", onmouseDown) 
-    canvas.addEventListener("mouseup", onMouseUp)
+    canvas.addEventListener("mousedown", startPainting) 
+    canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
